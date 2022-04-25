4895a88b88eda024d3c6015d5d6025275f13f6b4
Alexis Villegas <92242938+GOLFISHMANALEXIS@users.noreply.github.com>
2022-04-25 11:08:09 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert("Ya besame Karymita")
-    alert("Te voy a cuidar y amar mucho")
-    alert("❤")
-    
+    alert('Tal vez sea muy pronto, si lo sientes solo dimelo')
+    alert('Yo te esperaria 1000 años por estar contigo')
+    alert('Pero no quiero dejar pasar mucho tiempo y que sientas que no vamos a ningun lado')
+    alert('Yo te prometo que te voy cuidar y amar mucho')
+    alert('❤❤')
 });
 
 const noBtn = document.querySelector('#noBtn');
